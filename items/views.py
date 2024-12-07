6cd19295f6e616665bc4d8aadaab33ffa9d0f883
Shamit2105 <202301041@daiict.ac.in>
2024-12-07 18:07:27 +0530

    seller complete for now

--- items/views.py
+++ items/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView,UpdateView,DeleteView
 from .models import Item
 from .forms import ItemForm
+from django.urls import reverse_lazy
+
 
 class ItemListView(LoginRequiredMixin, ListView):
     model = Item
@@ -24,3 +26,19 @@ class ItemCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.seller = self.request.user
         return super().form_valid(form)
+
+class ItemUpdateView(LoginRequiredMixin,UpdateView):
+    model = Item
+    form_class = ItemForm
+    template_name = "item_update_seller.html"
+
+    def get_queryset(self):
+        return Item.objects.filter(seller=self.request.user)
+    
+class ItemDeleteView(LoginRequiredMixin, DeleteView):
+    model = Item
+    template_name = "item_delete_seller.html"
+    success_url = reverse_lazy('item_list')  # Redirect after deletion
+
+    def get_queryset(self):
+        return Item.objects.filter(seller=self.request.user)
