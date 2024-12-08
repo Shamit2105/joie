a182f14684c66b05fed4a79c66c8f13306a968f3
Shamit2105 <202301041@daiict.ac.in>
2024-12-08 07:22:05 +0530

    few chhanges

--- orders/forms.py
+++ orders/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Order
 from items.models import Item
+from django.shortcuts import redirect,get_object_or_404
 
 class OrderForm(forms.ModelForm):
     class Meta:
@@ -9,6 +10,29 @@ class OrderForm(forms.ModelForm):
 
     def clean_quantity(self):
         quantity = self.cleaned_data.get('quantity')
-        if quantity <= 0:
+        if quantity < 0:
             raise forms.ValidationError("Quantity must be greater than zero.")
         return quantity
+    def form_valid(self, form):
+        item_id = self.kwargs.get('pk')
+        item = get_object_or_404(Item, pk=item_id)
+
+        # Validate that requested quantity is not greater than available stock
+        requested_quantity = form.cleaned_data['quantity']
+        if requested_quantity > item.quantity:
+            form.add_error('quantity', 'Requested quantity exceeds available stock.')
+            return self.form_invalid(form)
+
+        # Update stock and save order
+        order = form.save(commit=False)
+        order.item = item
+        order.customer = self.request.user
+        order.total_price = item.price * requested_quantity
+
+        # Update item stock
+        item.quantity -= requested_quantity
+        item.save()
+
+        # Save order
+        order.save()
+        return redirect('customer_home')
